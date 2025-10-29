2ca996e158660edb07b0463c0b0bf20a3e43e46f
Luis Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2025-10-28 21:47:35 -0300

    feat: adiciona a funcionalidade de resetar o formulário

--- frontend/src/hooks/useForm.js
+++ frontend/src/hooks/useForm.js
@@ -1,14 +1,18 @@
 // useForm.js
-import { useState } from 'react';
+import { useState } from "react";
 
 const useForm = (initialState) => {
   const [formData, setFormData] = useState(initialState);
 
   const handleChange = (field, value) => {
-    setFormData({ ...formData, [field]: value });
+    setFormData((prev) => ({ ...prev, [field]: value }));
   };
 
-  return { formData, handleChange };
+  const resetForm = () => {
+    setFormData(initialState);
+  };
+
+  return { formData, handleChange, resetForm };
 };
 
 export default useForm;
