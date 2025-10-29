09c4f351f887fd204f85c6fb68308f84d8448274
Luis Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2025-10-28 21:47:14 -0300

    feat: transforma os componentes de checkbox em componentes controlados

--- frontend/src/components/Form/Fields/Preferences.js
+++ frontend/src/components/Form/Fields/Preferences.js
@@ -1,21 +1,17 @@
 // Preferences.js
 
-import React, { useState } from 'react';
-import Checkbox from '../../shared/Checkbox';
+import Checkbox from "../../shared/Checkbox";
 
 function Preferences({
   preferences,
   selectedPreferences = [],
   onPreferenceChange,
 }) {
-  const [currentPreferences, setCurrentPreferences] = useState(selectedPreferences)
-
   const handlePreferenceChange = (preference) => {
-    const updatedPreferences = currentPreferences.includes(preference)
-      ? currentPreferences.filter((pref) => pref !== preference)
-      : [...currentPreferences, preference];
+    const updatedPreferences = selectedPreferences.includes(preference)
+      ? selectedPreferences.filter((pref) => pref !== preference)
+      : [...selectedPreferences, preference];
 
-    setCurrentPreferences(updatedPreferences);
     onPreferenceChange(updatedPreferences);
   };
 
@@ -27,7 +23,7 @@ function Preferences({
           <li key={index} className="mb-2">
             <Checkbox
               value={preference}
-              checked={currentPreferences.includes(preference)}
+              checked={selectedPreferences.includes(preference)}
               onChange={() => handlePreferenceChange(preference)}
               className="text-blue-500"
             >
--- frontend/src/components/Form/Fields/RecommendationType.js
+++ frontend/src/components/Form/Fields/RecommendationType.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import Checkbox from '../../shared/Checkbox';
+import Checkbox from "../../shared/Checkbox";
 
-function RecommendationType({ onRecommendationTypeChange }) {
+function RecommendationType({ selectedType = "", onRecommendationTypeChange }) {
   return (
     <div className="mb-4">
       <h2 className="text-lg font-bold mb-2">Tipo de Recomendação:</h2>
@@ -10,15 +9,20 @@ function RecommendationType({ onRecommendationTypeChange }) {
           type="radio"
           name="recommendationType"
           value="SingleProduct"
-          onChange={() => onRecommendationTypeChange('SingleProduct')}
+          checked={selectedType === "SingleProduct"}
+          onChange={() => onRecommendationTypeChange("SingleProduct")}
           className="mr-2"
         />
-        <label htmlFor="SingleProduct" className="mr-4">Produto Único</label>
+        <label htmlFor="SingleProduct" className="mr-4">
+          Produto Único
+        </label>
+
         <Checkbox
           type="radio"
           name="recommendationType"
           value="MultipleProducts"
-          onChange={() => onRecommendationTypeChange('MultipleProducts')}
+          checked={selectedType === "MultipleProducts"}
+          onChange={() => onRecommendationTypeChange("MultipleProducts")}
           className="mr-2"
         />
         <label htmlFor="MultipleProducts">Múltiplos Produtos</label>
