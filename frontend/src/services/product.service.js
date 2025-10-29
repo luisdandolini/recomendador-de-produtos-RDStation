32d76530da88249fd7cd1efb1ce8c09e7bd0fa4e
Luis Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2025-10-28 22:19:27 -0300

    refactor: ajusta a variável de ambiente

--- frontend/src/services/product.service.js
+++ frontend/src/services/product.service.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = 'http://localhost:3001';
+const baseURL = process.env.REACT_APP_API_URL;
 
 const getProducts = async () => {
   try {
     const response = await axios.get(`${baseURL}/products`);
     return response.data;
   } catch (error) {
-    console.error('Erro ao obter os produtos:', error);
+    console.error("Erro ao obter os produtos:", error);
     throw error;
   }
 };
